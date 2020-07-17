5f344cfcd48c933617f3cf6a281ab58a2c1a7877
Aishath Nasir <aishath_n@hotmail.com>
2020-07-17 22:27:37 +0100

    created grid and base divs

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+function init() {
+
+  const grid = document.querySelector('.grid')
+  const cells = []
+  const width = 25
+  const gridCellCount = width * width
+
+  function createGrid() {
+    for (let i = 0; i < gridCellCount; i++) {
+      const cell = document.createElement('div')
+      cells.push(cell)
+      // cell.innerHTML = i
+      grid.appendChild(cell)
+    }
+  }
+
+  createGrid()
+
+
+}
+document.addEventListener('DOMContentLoaded', init)
